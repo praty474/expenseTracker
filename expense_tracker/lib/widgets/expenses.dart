1c9201937ad450be1a33e76cfa80e820b0fe82c7
praty474 <p.kayastha.pk@gmail.com>
2023-07-01 21:37:06 +0545

    ep12

---
+++ expense_tracker/lib/widgets/expenses.dart
@@ -0,0 +1,50 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter',
+        amount: 12,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+        title: 'Cinema',
+        amount: 122,
+        date: DateTime.now(),
+        category: Category.education),
+  ];
+  void _openAddExpenseOverlay(){
+showModalBottomSheet(context: context, builder: builder)
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Expense tracker'), actions: [
+        IconButton(
+          onPressed: _openAddExpenseOverlay,
+          icon: const Icon(
+            Icons.add,
+          ),
+        ),
+      ]),
+      body: Column(
+        children: [
+          const Text('data'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
